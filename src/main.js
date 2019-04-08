9b276baa408b6a4222a65543a21c98a6329e6cdb
Antonija Pek <antonijapek@gmail.com>
2019-04-08 21:29:04 +0300

    Basic fetching

--- src/main.js
+++ src/main.js
@@ -7,12 +7,20 @@ import { config } from "./config";
 import { firestorePlugin } from "vuefire";
 import firebase from "firebase/app";
 import BootstrapVue from "bootstrap-vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fal } from "@fortawesome/pro-light-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fal);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(firestorePlugin);
 Vue.use(BootstrapVue);
 
 firebase.initializeApp(config);
 export const db = firebase.firestore();
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,56 @@
     <div v-for="(item, index) in ads" :key="index">
       <div>
         <b-card :title="item.name" :sub-title="item.age">
-          <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, cupiditate!</b-card-text>
-          <b-button variant="primary">Primary</b-button>
+          <img :src="item.image" :alt="item.name">
+          <b-card-text>{{ item.description }}</b-card-text>
+          <b-card-text>{{ item.height }} rast</b-card-text>
+          <span v-if="item.vaccines">
+            <span v-if="item.vaccines" v-b-tooltip.hover title="Cijepljena">
+              <font-awesome-icon :icon="['fal', 'syringe']"/>
+            </span>
+            <span v-else v-b-tooltip.hover title="Cijepljen">
+              <font-awesome-icon :icon="['fal', 'syringe']"/>
+            </span>
+          </span>
+          <span v-if="item.chip">
+            <span v-if="item.vaccines" v-b-tooltip.hover title="Cipirana">
+              <font-awesome-icon :icon="['fal', 'microchip']"/>
+            </span>
+            <span v-else v-b-tooltip.hover title="Cipiran">
+              <font-awesome-icon :icon="['fal', 'microchip']"/>
+            </span>
+          </span>
+          <span v-if="item.sterilization">
+            <span v-if="item.vaccines" v-b-tooltip.hover title="Sterilizirana">
+              <font-awesome-icon :icon="['fal', 'syringe']"/>
+            </span>
+            <span v-else v-b-tooltip.hover title="Steriliziran">
+              <font-awesome-icon :icon="['fal', 'syringe']"/>
+            </span>
+          </span>
+          <span v-if="item.castration">
+            <span v-if="item.vaccines" v-b-tooltip.hover title="Kastrirana">
+              <font-awesome-icon :icon="['fal', 'syringe']"/>
+            </span>
+            <span v-else v-b-tooltip.hover title="Kastrirana">
+              <font-awesome-icon :icon="['fal', 'syringe']"/>
+            </span>
+          </span>
         </b-card>
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+span > span {
+  padding: 5px;
+  color: orangered;
+  width: 32px;
+  height: 32px;
+}
+</style>
+
 
 <script>
 import { db } from "../main";
