0645ec57c8e25be1ac01603c5051328c08984182
Antonija <antonija.pek@nosto.com>
2019-11-28 16:43:17 +0200

    filtering

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,16 +2,17 @@
   <div>
     <h1>Search</h1>
     <div v-show="!getPaginationLoading">
-      <input v-model="animalTypeFilter" type="radio" value="cat" /> Cat
-      <input v-model="animalTypeFilter" type="radio" value="dog" /> Dog
-      <input v-model="animalTypeFilter" type="radio" value="bird" /> Bird
-      <input v-model="animalTypeFilter" type="radio" value="rodent" /> Rodent
+      <input v-model="animalType" type="radio" value="cat" /> Cat
+      <input v-model="animalType" type="radio" value="dog" /> Dog
+      <input v-model="animalType" type="radio" value="bird" /> Bird
+      <input v-model="animalType" type="radio" value="rodent" /> Rodent
       <br />
       <br />
       <input v-model="filters" value="vaccinated" type="checkbox" /> Vaccinated
       <input v-model="filters" value="chipped" type="checkbox" /> Chipped
       <input v-model="filters" value="sterilized" type="checkbox" /> Sterilized
       <hr />
+      {{ animalType }}
       <div v-for="(ad, index) in filteredData" :key="index + '-a'">
         <router-link
           :to="{
@@ -44,7 +45,7 @@ export default {
   data() {
     return {
       filterCats: false,
-      animalTypeFilter: "",
+      animalType: "",
       filters: []
     };
   },
@@ -52,36 +53,31 @@ export default {
     ...mapGetters("petAds", ["getPaginationLoading", "getCurrentAds"]),
     // Check if an item has all items in this.filters and they must be true
     filteredData() {
-      return this.getCurrentAds.filter(item => {
-        if (
-          this.filters.length >= 0 &&
-          item.animalType === this.animalTypeFilter
-        ) {
-          console.log("No filters, returning an item");
-          return item;
-        } else {
-          Object.keys(item).map(key => {
-            this.filters.map(filter => {
-              if (key.includes(filter)) {
-                return item;
-              }
-            });
-          });
-        }
+      return this.getCurrentAds.filter(i => {
+        return this.filters.every(key => i[key]);
       });
     }
   },
   created() {
-    // Assign animalTypeFilter if there is query for that in URL
+    // Assign animalType if there is query for that in URL
     this.$store.dispatch("petAds/fetchAds", 20);
-    if (this.$route.query.animalType) {
-      this.animalTypeFilter = this.$route.query.animalType;
-    }
+    this.$route.query.animalType
+      ? (this.animalType = this.$route.query.animalType)
+      : "";
   },
   methods: {
     getMoreAds() {
       this.$store.dispatch("petAds/fetchAds", 20);
     }
   }
+  // watch: {
+  //     animalType(newVal) {
+  //       this.$router.push({ query: { animalType: newVal }});
+  //     },
+  //     '$route.query.animalType': function(val) {
+  //         this.animalType = val;
+  //         this.$store.dispatch("petAds/fetchAds", 20);
+  //     }
+  // }
 };
 </script>
