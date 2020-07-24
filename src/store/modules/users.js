24dda38753448499de4f427ae90bdd91e56df8b6
antonija <antonijapek@gmail.com>
2020-07-24 16:25:01 +0300

    will not write duplicate user

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -15,7 +15,6 @@ const users = {
     authenticated(state) {
       let authenticated = false;
       let user = state.user;
-      console.log(user);
       if (user !== null && user !== false && user !== "") {
         authenticated = true;
       }
@@ -25,50 +24,47 @@ const users = {
   mutations: {
     SAVE_USER(state, payload) {
       state.user = payload;
-      sessionStorage.setItem("user", JSON.stringify(payload));
-      console.log(
-        "%c LOGIN SUCCESFULL ",
-        "color: gray; background: #42f598; padding: 5px; font-size: 12px; border-radius: 15px;"
-      );
     },
     REMOVE_USER(state) {
-      sessionStorage.removeItem("user");
       state.user = null;
-      console.log(
-        "%c LOGOUT SUCCESFULL ",
-        "color: gray; background: #42ddf5; padding: 5px; font-size: 12px; border-radius: 15px;"
-      );
     }
   },
   actions: {
-    fetchUser(payload) {
-      console.log(payload);
-    },
+    // fetchUser(payload) {
+    //   // User fetched, data in payload
+    // },
     googleLogin({ commit }) {
       Firebase.auth()
         .signInWithPopup(new Firebase.auth.GoogleAuthProvider())
         .then(result => {
-          console.log("user", result.user);
-          // 1 - Check if email exists in database
-
-          // 2 - Write new document
           const db = Firebase.firestore();
+          // 1 - Check if email exists in database
           db.collection("users")
-            .add({
-              name: result.user.displayName,
-              email: result.user.email,
-              created: new Date(),
-              modified: new Date()
-            })
-            .then(() => {
-              console.log("Document successfully written!");
-            })
-            .catch(error => {
-              console.error("Error writing document: ", error);
-            });
+            .where("email", "==", result.user.email)
+            .get()
+            .then(function(querySnapshot) {
+              if (querySnapshot.docs.length === 0) {
+                // 2 - Write new document
+                db.collection("users")
+                  .add({
+                    name: result.user.displayName,
+                    email: result.user.email,
+                    created: new Date(),
+                    modified: new Date()
+                  })
+                  .then(() => {
+                    console.log("Document successfully written!");
+                  })
+                  .catch(error => {
+                    console.error("Error writing document: ", error);
+                  });
 
-          commit("SAVE_USER", result.user);
-          // 3 - commit to vuex
+                commit("SAVE_USER", result.user);
+              }
+            })
+            .catch(function(error) {
+              console.log("Error getting documents: ", error);
+            });
         })
         .catch(function(error) {
           const errorCode = error.code;
